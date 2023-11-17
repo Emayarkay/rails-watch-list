bbc42cbd22e0caeed131e24a652bd7a816363f9e
Mark Watkinson <markjwatkinson@gmail.com>
2023-11-17 17:29:36 +0000

    more styles

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+
 
   validates :name, presence: true
   validates :name, uniqueness: true
